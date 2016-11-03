62e499070458c6ccb0011ed19a55550ef33c919d
Dimitrios Apostal <idietmoran@gmail.com>
2016-11-02 23:27:17 -0400

    init

--- src/gui/ffxiv_qt.cpp
+++ src/gui/ffxiv_qt.cpp
@@ -14,11 +14,20 @@ FFXIV_QT::~FFXIV_QT()
 
 void FFXIV_QT::on_button_translate_clicked()
 {
-	ui.output->setText("clicked");
+	// QString -> STD string
+	QString inputText = ui.input->toPlainText();
+	// Get translation
+	std::string transitionText = translate(inputText.toStdString());
+	// STD string -> QString
+	QString finalString = QString::fromStdString(transitionText);
+
+	// display our message
+	ui.output->setText(finalString);
 }
 
 void FFXIV_QT::on_button_clear_clicked()
 {
+	// Clear all fields
 	ui.input->setText("");
 	ui.output->setText("");
 }
--- src/translate.cpp
+++ src/translate.cpp
@@ -1,4 +1,4 @@
-// translate.cpp
+﻿// translate.cpp
 
 #include <string>
 #include <map>
@@ -15,8 +15,34 @@ string translate(string x)
 
 	// create our dictionary
 	map<char, char> dictionary;
-	dictionary['a'] = 'z';
-	dictionary['b'] = 'y';
+	/*NOTE: */
+	dictionary['a'] = 0xE071;
+	dictionary['b'] = 0xE072;
+	dictionary['c'] = 0xE073;
+	dictionary['d'] = 0xE074;
+	dictionary['e'] = 0xE075;
+	dictionary['f'] = 0xE076;
+	dictionary['g'] = 0xE077;
+	dictionary['h'] = 0xE078;
+	dictionary['i'] = 0xE079;
+	dictionary['j'] = 0xE07A;
+	dictionary['k'] = 0xE07B;
+	dictionary['l'] = 0xE07C;
+	dictionary['m'] = 0xE07D;
+	dictionary['n'] = 0xE07E;
+	dictionary['o'] = 0xE07F;
+	dictionary['p'] = 0xE080;
+	dictionary['q'] = 0xE081;
+	dictionary['r'] = 0xE082;
+	dictionary['s'] = 0xE083;
+	dictionary['t'] = 0xE084;
+	dictionary['u'] = 0xE085;
+	dictionary['v'] = 0xE086;
+	dictionary['w'] = 0xE087;
+	dictionary['x'] = 0xE088;
+	dictionary['y'] = 0xE089;
+	dictionary['z'] = 0xE08A;
+	dictionary[' '] = ' ';
 
 	// initialize our final string
 	string output;
