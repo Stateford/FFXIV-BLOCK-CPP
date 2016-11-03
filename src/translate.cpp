e097e3bf44d4801f6d1d4155e9dcbc07b8ec6803
Dimitrios Apostal <idietmoran@gmail.com>
2016-11-02 23:52:08 -0400

    added dictionary check

--- src/translate.cpp
+++ src/translate.cpp
@@ -16,33 +16,32 @@ string translate(string x)
 	// create our dictionary
 	map<char, char> dictionary;
 	/*NOTE: */
-	dictionary['a'] = 0xE071;
-	dictionary['b'] = 0xE072;
-	dictionary['c'] = 0xE073;
-	dictionary['d'] = 0xE074;
-	dictionary['e'] = 0xE075;
-	dictionary['f'] = 0xE076;
-	dictionary['g'] = 0xE077;
-	dictionary['h'] = 0xE078;
-	dictionary['i'] = 0xE079;
-	dictionary['j'] = 0xE07A;
-	dictionary['k'] = 0xE07B;
-	dictionary['l'] = 0xE07C;
-	dictionary['m'] = 0xE07D;
-	dictionary['n'] = 0xE07E;
-	dictionary['o'] = 0xE07F;
-	dictionary['p'] = 0xE080;
-	dictionary['q'] = 0xE081;
-	dictionary['r'] = 0xE082;
-	dictionary['s'] = 0xE083;
-	dictionary['t'] = 0xE084;
-	dictionary['u'] = 0xE085;
-	dictionary['v'] = 0xE086;
-	dictionary['w'] = 0xE087;
-	dictionary['x'] = 0xE088;
-	dictionary['y'] = 0xE089;
-	dictionary['z'] = 0xE08A;
-	dictionary[' '] = ' ';
+	dictionary['a'] = '';
+	dictionary['b'] = '';
+	dictionary['c'] = '';
+	dictionary['d'] = '';
+	dictionary['e'] = '';
+	dictionary['f'] = '';
+	dictionary['g'] = '';
+	dictionary['h'] = '';
+	dictionary['i'] = '';
+	dictionary['j'] = '';
+	dictionary['k'] = '';
+	dictionary['l'] = '';
+	dictionary['m'] = '';
+	dictionary['n'] = '';
+	dictionary['o'] = '';
+	dictionary['p'] = '';
+	dictionary['q'] = '';
+	dictionary['r'] = '';
+	dictionary['s'] = '';
+	dictionary['t'] = '';
+	dictionary['u'] = '';
+	dictionary['v'] = '';
+	dictionary['w'] = '';
+	dictionary['x'] = '';
+	dictionary['y'] = '';
+	dictionary['z'] = '';
 
 	// initialize our final string
 	string output;
@@ -50,8 +49,14 @@ string translate(string x)
 	// loop through all characters in our lowercase string
 	for (char i : lowercase)
 	{
-		// add translated character to final string
-		output += dictionary[i];
+		// check if character exists in our dictionary
+		if (dictionary[i] != '\0') {
+			// add translated character to final string
+			output += dictionary[i];
+		}
+		else {
+			output += i;	// if char is not in our dictionary add our regular character to string
+		}
 	}
 
 	// return our translation
